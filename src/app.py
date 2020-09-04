557fa2445baee65032a106828bef51726cae89d0
野口大輝 <noguchi@cps.im.dendai.ac.jp>
2020-09-04 15:43:44 +0900

    コンテツ内容追記0

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello world!!\nHowabout"
+    return "Hello world!!\nH"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
